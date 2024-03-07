8268bedcb8cb0a0adc2376f5e90b6c21a84e95bb
Oleksandr Lazurenko <alexprofteach@gmail.com>
2024-03-07 09:38:04 +0000

    Added the language button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import MyTabs from "./components/MyTabs";
-import Header from "./components/Header";
+import React, { useState } from "react";
+import AppEn from "./components/AppEn";
+import AppDe from "./components/AppDe";
+import AppUkr from "./components/AppUkr";
+
 import "./styles.css";
 
 export default function App() {
+  const [language, setLanguage] = useState("en");
+
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
+  };
+
   return (
     <div className="App">
-      <div className="maincontainer">
-        <Header />
-        <MyTabs />
-      </div>
+      <select value={language} onChange={handleLanguageChange}>
+        <option value="en">EN</option>
+        <option value="de">DE</option>
+        <option value="ukr">UKR</option>
+      </select>
+      {language === "en" && <AppEn />}
+      {language === "de" && <AppDe />}
+      {language === "ukr" && <AppUkr />}
     </div>
   );
 }
---
+++ src/components/AppDe.js
@@ -0,0 +1,14 @@
+import MyTabs from "./MyTabs";
+import Header from "./Header";
+
+export default function AppDe() {
+  return (
+    <div>
+      <div className="maincontainer">
+        <Header />
+        <MyTabs />
+        <h1>Hallo</h1>
+      </div>
+    </div>
+  );
+}
