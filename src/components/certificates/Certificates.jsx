2b78fdc4a8f924de805ab38cafe6d78b2e3cd3de
Oleksandr Lazurenko <alexprofteach@gmail.com>
2024-03-07 03:59:00 +0000

    Worked on the pdf

--- src/components/certificates/Certificates.jsx
+++ src/components/certificates/Certificates.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
 const Certificates = () => {
-  const pdfUrl =
-    "https://www.dropbox.com/scl/fi/02kc0060g1nmwz4e1b3ja/EnglishScore.pdf?rlkey=c4crjy32y62xin42527n25qhv&dl=0";
-
   return (
     <div>
-      <iframe title="pdf-viewer" src={pdfUrl} width="100%" height="600px" />
+      <h2>Language certificates</h2>
+      <a
+        data-flickr-embed="true"
+        href="https://drive.google.com/file/d/1vMSrZx9M2F87caJ3zt8LUSnPluMWPr7p/view?usp=sharing"
+        title="Englishscore"
+        target="_blank"
+      >
+        <img
+          src="https://live.staticflickr.com/65535/53571670742_708e05efdc.jpg"
+          width="354"
+          height="500"
+          alt="Englishscore"
+        />
+      </a>
+      <script
+        async
+        src="//embedr.flickr.com/assets/client-code.js"
+        charset="utf-8"
+      ></script>
     </div>
   );
 };
