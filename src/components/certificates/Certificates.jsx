a251359180f00f8a9a56a2860b44a6ed9259d236
Oleksandr Lazurenko <alexprofteach@gmail.com>
2024-03-07 03:25:44 +0000

    Try Dropbox

--- src/components/certificates/Certificates.jsx
+++ src/components/certificates/Certificates.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Certificates = () => {
-  const pdfUrl = "https://app.box.com/s/foncbqebdazudbc8ystvclnijz9rf856";
+  const pdfUrl =
+    "https://www.dropbox.com/scl/fi/02kc0060g1nmwz4e1b3ja/EnglishScore.pdf?rlkey=c4crjy32y62xin42527n25qhv&dl=0";
 
   return (
     <div>
