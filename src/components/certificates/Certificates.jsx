2056a8da0d240d4db1a5686fcfef71a9a2a6d5f2
Oleksandr Lazurenko <alexprofteach@gmail.com>
2024-03-07 04:59:15 +0000

    Add cloud storage

--- src/components/certificates/Certificates.jsx
+++ src/components/certificates/Certificates.jsx
@@ -5,13 +5,12 @@ const Certificates = () => {
     <div>
       <h2>Language certificates</h2>
       <a
-        data-flickr-embed="true"
-        href="https://drive.google.com/file/d/1vMSrZx9M2F87caJ3zt8LUSnPluMWPr7p/view?usp=sharing"
+        href="https://storage.cloud.google.com/alexprof/PDFs/EnglishScore.pdf"
         title="Englishscore"
         target="_blank"
       >
         <img
-          src="https://live.staticflickr.com/65535/53571670742_708e05efdc.jpg"
+          src="https://fffb5bd2d1bee9d35bda342ea6da6ea369aa5c8687ccedc4730a0b4-apidata.googleusercontent.com/download/storage/v1/b/alexprof/o/Images%2FEnglishscore.jpg?jk=ATxpoHdoxxqTuhV4DSq-G-9nY3PfGdiJ-8yBfN9M-b-MxS3IKflJXVdgee49LgytO3ytE-UelZPVU5d-J--Ah8s4iUSDo2TkXFD0H9j1IHTMjbv9_mLjfVrX1OrxDitc2Z4cgyVhMLNfAFmm9BszeEi0s-SGEdIe7Rml6ho_VfwyTTjt70xUzlEh7T_4T8NeBjIGGXjwljk0vEVZZkT4f1BBg0eQ9HcnbZ0597ar_iBoyINP14p2spQyFFHim-KjDLFYScKeRXK-MpNwM_4YYXNnZT3blVxcP_Ba9UIIcKNib4AoRac17BHO6R2Orc3ppQGm5XIgpcWHiga-hDvjEFY51XZBtmG2CvDKvg68BleOWnBgDAVDkuNasNIVWv0FRgouTMGVs2cJrC1CgxmyRXmOuQsdQTNKrV7Mi7nyc-dzWuHdaakrwnQ0b4V6odpDPQO9dx10yM8jSiDMAXOBOxL3DvfAT-kw9OXNoyWHPIXuP_Ez_cToRrmEwSpo5neZOjNMNHbJNVeADeOZV2EX3LsAOo_HRPYXZ1B1JcmcZcedllqTHaANw3y3jb_Xwb37pncQGKyPBCnDfGgxDoMNKApacaQIHN6pHGFfIZRLUTgiLszlz3weajBtSdsEC0tceVpxM0MB6hU8vcH5_g6FKaOaKCSqkzGJcFeaId4L_a7qN7j3XT6dV4RjSn-m3XAmCnqmARRAu6G9ozayuptcignUyg2S62LAeQiL4m18VzUIq0ZwPVfo6WsMQVh3cBw6CvwjGUkvUlqgUfjggsuVlt8dD7zG74YD9b4T6ozhv41J-gp3IH5rvoQnzUP3Rs3mQfjuqI0o3pxC3uO_SwxdjLM6SnUuwdDWQK_M65JUkBeUyzaq9imWw9fw9Omt934TNDHDKikXcIwEB6MrIGdiFeTFXIwNIoa_0lL7ExKRcuIM-ca7Wv6Y971jCr7E8_dHeb3_rogVQGLMHaoX-0ykVsQPxq8bhLvL9SL0AH4ZJOcUQL-HlxOHVSPhELMQbdACoplYRe4oSC35XUGjyydtRs_qwdv0fbW6a65PHz3vsLOLOrt65LxsvaSzdBSXektSrn_ihx7Ea_Vt6UNVUJ236s67tF0qPsoC_GeQaRE9LA&isca=1"
           width="354"
           height="500"
           alt="Englishscore"
